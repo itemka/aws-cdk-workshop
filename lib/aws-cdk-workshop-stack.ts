82fb55c194f0213b1c16984d07543a4cc075c083
Artsem Paulenka <itemka2503@gmail.com>
2023-10-27 01:02:08 +0200

    defines an API Gateway REST API resource backed by our hello function

--- lib/aws-cdk-workshop-stack.ts
+++ lib/aws-cdk-workshop-stack.ts
@@ -1,6 +1,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 
 export class AwsCdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -11,6 +12,11 @@ export class AwsCdkWorkshopStack extends Stack {
       runtime: lambda.Runtime.NODEJS_16_X,
       code: lambda.Code.fromAsset('lambda'),
       handler: 'hello.handler',
+    })
+
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello
     })
   }
 }
